edbacb24cfa64a1c7a56c222b5d482c35c303fd1
itsmattpaw <itsmattpaw@gmail.com>
2022-03-13 11:14:44 -0400

    Store Class Create created store class with initialize method and class variable all with reader method

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
+class Store  
+    attr_accessor :idnum, :address, :city, :state, :zip 
+    @@all = []
+
+    def initialize(idnum:,address:,city:,state:,zip:)
+        @idnum = idnum
+        @address = address
+        @city = city
+        @state = state
+        @zip = zip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
