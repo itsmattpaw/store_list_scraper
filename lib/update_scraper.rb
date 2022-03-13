6f8a427700adf861e9dd0f2c57c86abd25516db4
itsmattpaw <itsmattpaw@gmail.com>
2022-03-13 10:59:29 -0400

    create LocationScraper class and initialize method

--- lib/update_scraper.rb
+++ lib/update_scraper.rb
@@ -2,3 +2,16 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+class LocationScraper 
+    attr_accessor :link, :state_pages, :city_pages, :loc_pages
+    attr_reader :base
+
+    def initialize(link)
+        @link = link
+        @base = 'https://storefound.org/'
+        @state_pages = []
+        @city_pages = []
+        @loc_pages = []
+    end
+    
+end
