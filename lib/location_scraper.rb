ab41194e1a440c179e25498f60051918a5b6e9a6
itsmattpaw <itsmattpaw@gmail.com>
2022-03-13 11:26:51 -0400

    LocationScraper created page scrape method to find links on main pages

--- lib/location_scraper.rb
+++ lib/location_scraper.rb
@@ -14,4 +14,19 @@ class LocationScraper
         @loc_pages = []
     end
 
+    def page_scrape(page)
+        doc = Nokogiri::HTML5(URI.open(page))
+        doc.css(".main-block a").each do |lk| #pull all links from main body 
+          j = lk.attribute("href").text #look at only the url text
+          case j.split("/").length 
+          when 3 
+            @state_pages << j
+          when 4
+            @city_pages << j
+          when 5
+            @loc_pages << j
+          end
+        end
+      end
+
 end
